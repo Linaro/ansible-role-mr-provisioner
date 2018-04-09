6a21785ce154b9f13aed0fabc04087271b63deaf
Baptiste Gerondeau <bgerondeau@gmail.com>
2018-04-09 14:17:03 +0100

    Added modify capabilities to preseed module, and refactored code in Object.
    Changed known_good and public defaults back to true
    
    Fixed portability issues with python3. Use 'except Error as e' instead of 'except Error, e'.
    
    Fixed portability issues with name imports. Urllib is and was a mess.
    
    Same fix for image module
    
    Ran PyLint and corrected some formatting mistakes, removed unnecessary elses and changed opening file bit.
    
    Changed /dev/null to empty string, except in YAML
    
    I have seen the error of my ways, preseed path is now empty string by default
    
    Refactored Preseed_Uploader

--- library/mr_provisioner_preseed.py
+++ library/mr_provisioner_preseed.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import json
 import requests
-
-from future.standard_library import install_aliases
-install_aliases()
-
-from urllib.parse import urljoin
+try:
+    from urllib.parse import urljoin
+except ImportError:
+    from urlparse import urljoin
 
 ANSIBLE_METADATA = {
     'metadata_version': '1.1',
@@ -17,9 +17,7 @@ ANSIBLE_METADATA = {
 DOCUMENTATION = '''
 ---
 module: mr-provisioner-preseed
-
 short_description: Manage preseed files in Mr. Provisioner
-
 description:
     Implemented:
         - Upload new preseed
@@ -27,7 +25,6 @@ description:
     Not implemented:
         - modifying existing preseed
         - deleting existing preseed
-
 options:
     name:
         description:
@@ -52,9 +49,9 @@ options:
     public:
         description: Mark public. Default false.
         required: false
-
 author:
     - Jorge Niedbalski <jorge.niedbalski@linaro.org>
+    - Baptiste Gerondeau <baptiste.gerondeau@linaro.org>
 '''
 
 EXAMPLES = '''
@@ -63,6 +60,12 @@ EXAMPLES = '''
   path: ./preseeds/moonshot-generic.preseed.txt
   url: http://172.27.80.1:5000/
   token: "{{ provisioner_auth_token }}"
+
+# Uses existing file from MrProvisioner
+- name: test_preseed
+  path: ''
+  url: http://172.0.0.1:5000
+  token: "{{ fancy_token }}"
 '''
 
 RETURN = '''
@@ -77,15 +80,119 @@ RETURN = '''
 
 from ansible.module_utils.basic import AnsibleModule
 
+class ProvisionerError(Exception):
+    def __init__(self, message):
+        super(ProvisionerError, self).__init__(message)
+
+class PreseedUploader(object):
+    """ This class handles the job of uploading a preseed file to MrP.
+        It shall only be called if there is a file to be uploaded, else you can
+        fetch the thing via the regular call in the Ansible role"""
+    def __init__(self, mrp_url, mrp_token, preseed_file, preseed_name,
+                 preseed_type, preseed_desc='', preseed_knowngood=False,
+                 preseed_public=False):
+        self.url = mrp_url
+        self.authhead = {'Authorization': mrp_token}
+        self.file = preseed_file
+        self.name = preseed_name
+        self.type = preseed_type
+        self.id = None
+        self.desc = preseed_desc
+        self.knowngood = preseed_knowngood
+        self.public = preseed_public
+
+    def _check_for_existence(self):
+        url = urljoin(self.url, '/api/v1/preseed?show_all=true')
+        r = requests.get(url, headers=self.authhead)
+        if r.status_code != 200:
+            raise ProvisionerError('Error fetching {}, HTTP {} {}'.format(url,
+                                                r.status_code, r.reason))
+
+        for preseed in r.json():
+            if preseed['name'] == self.name:
+                self.id = preseed['id']
+                return True
+
+        return False
+
+    def _get_preseed_from_file(self):
+        json_preseed = {}
+        contents = ''
+
+        with open(self.file, 'r') as fd:
+            lines = fd.readlines()
+
+        for line in lines:
+            contents += line
+
+        json_preseed['content'] = contents
+        json_preseed['name'] = self.name
+        json_preseed['type'] = self.type
+        json_preseed['public'] = self.public
+        json_preseed['known_good'] = self.knowngood
+        if self.desc != '':
+            json_preseed['description'] = self.desc
+
+        return json_preseed
+
+    def upload_preseed(self):
+        """Uploads preseed. Should check first that preseed doesn't exist, else
+        it modifies preseed (separate function ?). Post to upload, Put to
+        modify (+ id). Maybe implement a jinja2 syntax check ? But that should
+        be done on mrp's side"""
+        res = {}
+        try:
+            exists = self._check_for_existence()
+        except ProvisionerError as e:
+            res['error'] = str(e)
+            return res
+
+        if not exists and self.file == '':
+            res['error'] = 'Preseed does not exist and file not given'
+            return res
+
+        if self.id != None and self.file != '':     #Exists and file given
+            try:
+                res = self._modify_preseed(method='PUT')
+            except ProvisionerError as e:
+                res['error'] = str(e)
+            return res
+        elif self.file != '':        #Doesn't exist and file given
+            try:
+                res = self._modify_preseed(method='POST')
+            except ProvisionerError as e:
+                res['error'] = str(e)
+            return res
+        else:       #Exists and file not given, is it useful fetching contents?
+            return res
+
+    def _modify_preseed(self, method):
+        preseed = self._get_preseed_from_file()
+        url = urljoin(self.url, '/api/v1/preseed')
+        if method == 'PUT':
+            if self.id == None:
+                raise ProvisionerError('preseed ID is undefined, please use upload_preseed')
+            url_id = '/api/v1/preseed/' + str(self.id)
+            url = urljoin(self.url, url_id)
+            r = requests.put(url, headers=self.authhead, data=json.dumps(preseed))
+            if r.status_code != 200:
+                raise ProvisionerError('Error putting preseed {} at ID {}, \
+                                   HTTP {} {}'.format(self.name, url, r.status_code, r.reason))
+        elif method == 'POST':
+            r = requests.post(url, headers=self.authhead, data=json.dumps(preseed))
+            if r.status_code != 201:
+                raise ProvisionerError('Error posting preseed {}, \
+                                       HTTP {} {}'.format(self.name, r.status_code, r.reason))
+        else:
+            raise ProvisionerError('Bad _modify_preseed call')
+        return r.json()
 
 def run_module():
-    # define the available arguments/parameters that a user can pass to
-    # the module
     module_args = dict(
-        description=dict(type='str', required=False, default=""),
+        description=dict(type='str', required=False, default=''),
         name=dict(type='str', required=True),
-        type=dict(type='str', required=False, default="preseed"),
-        path=dict(type='str', required=True),
+        type=dict(type='str', required=False, default='preseed'),
+        path=dict(type='str', required=False, default=''),
         url=dict(type='str', required=True),
         token=dict(type='str', required=True),
         known_good=dict(type='bool', required=False, default=False),
@@ -93,55 +200,36 @@ def run_module():
     )
 
     result = dict(
-        changed=False
+        changed=False,
+        debug={},
     )
 
     module = AnsibleModule(
         argument_spec=module_args,
-        supports_check_mode=True
+        supports_check_mode=True,
     )
 
     if module.check_mode:
         return result
-    # Determine if image is already uploaded
-    headers = {'Authorization': module.params['token']}
-    url = urljoin(module.params['url'], "/api/v1/preseed?show_all=true")
-    r = requests.get(url, headers=headers)
-    if r.status_code != 200:
-        module.fail_json(msg='Error fetching {}, HTTP {} {}'.format(url,
-                         r.status_code, r.reason), **result)
-    for preseed in r.json():
-        if preseed['name'] == module.params['name']:
-            result['json'] = preseed
-            module.exit_json(**result)
 
-    headers = {'Authorization': module.params['token']}
-    url = urljoin(module.params['url'], "/api/v1/preseed")
+    uploader = PreseedUploader(module.params['url'],
+    module.params['token'], module.params['path'],
+    module.params['name'], module.params['type'],
+    module.params['description'], module.params['known_good'],
+    module.params['public'])
 
-    with open(module.params['path'], 'r') as content:
-        content = content.read()
+    try:
+        res = uploader.upload_preseed()
+    except ProvisionerError as e:
+        module.fail_json(msg=str(e), **result)
 
-    data = json.dumps({
-        'description': module.params['description'],
-        'type': module.params['type'],
-        'name': module.params['name'],
-        'known_good': module.params['known_good'],
-        'public': module.params['public'],
-        'content': content,
-    })
+    if 'error' in res:
+        module.fail_json(msg=res['error'], **result)
 
-    r = requests.post(url, data=data, headers=headers)
-    if r.status_code != 201:
-        msg = ("Error fetching {}, HTTP {} {}\nrequest data: {}\nresult json: {}".
-                format(url, r.status_code, r.reason, data, r.json()))
-        module.fail_json(msg=msg, **result)
-    result['json'] = r.json()
+    result['json'] = res
     result['changed'] = True
 
-    # in the event of a successful module execution, you will want to
-    # simple AnsibleModule.exit_json(), passing the key/value results
     module.exit_json(**result)
-
 
 def main():
     run_module()
